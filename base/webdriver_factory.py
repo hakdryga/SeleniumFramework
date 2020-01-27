af0fb4b9ed10911ec93c25192bec7cf1bd2a9779
Anna Drygalska <hakdryga@gmail.com>
2020-01-27 13:17:39 +0100

    safari driver added

--- base/webdriver_factory.py
+++ base/webdriver_factory.py
@@ -9,7 +9,9 @@ class WebDriverFactory:
         base_url = "https://letskodeit.teachable.com/"
         if self.browser == "firefox":
             driver = webdriver.Firefox()
-        elif self.browser == "chrome":
+        if self.browser == "safari":
+            driver = webdriver.Safari()
+        if self.browser == "chrome":
             driver = webdriver.Chrome()
         else:
             driver = webdriver.Chrome()
